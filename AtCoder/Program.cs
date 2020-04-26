41dc7df214f06bc3fa81bc3093aab04d77bfbc17
rui.kanoh <rui.kanoh@gmail.com>
2020-04-26 13:55:01 +0900

    WA

--- AtCoder/Program.cs
+++ AtCoder/Program.cs
@@ -22,6 +22,142 @@ namespace AtCoder
 
 	public class Question
 	{
+		public static int Dice1(char c)
+		{
+			int ans;
+			switch (c) {
+				case 'E':
+				default:
+					ans = 4;
+					break;
+				case 'N':
+					ans = 2;
+					break;
+				case 'S':
+					ans = 5;
+					break;
+				case 'W':
+					ans = 3;
+					break;
+			}
+
+			return ans;
+		}
+
+		public static int Dice2(char c)
+		{
+			int ans;
+			switch (c) {
+				case 'E':
+				default:
+					ans = 4;
+					break;
+				case 'N':
+					ans = 6;
+					break;
+				case 'S':
+					ans = 1;
+					break;
+				case 'W':
+					ans = 3;
+					break;
+			}
+
+			return ans;
+		}
+
+		public static int Dice3(char c)
+		{
+			int ans;
+			switch (c) {
+				case 'E':
+				default:
+					ans = 2;
+					break;
+				case 'N':
+					ans = 6;
+					break;
+				case 'S':
+					ans = 1;
+					break;
+				case 'W':
+					ans = 5;
+					break;
+			}
+
+			return ans;
+		}
+
+		public static int Dice4(char c)
+		{
+			int ans;
+			switch (c) {
+				case 'E':
+				default:
+					ans = 6;
+					break;
+				case 'N':
+					ans = 2;
+					break;
+				case 'S':
+					ans = 5;
+					break;
+				case 'W':
+					ans = 1;
+					break;
+			}
+
+			return ans;
+		}
+
+		public static int Dice5(char c)
+		{
+			int ans;
+			switch (c) {
+				case 'E':
+				default:
+					ans = 4;
+					break;
+				case 'N':
+					ans = 1;
+					break;
+				case 'S':
+					ans = 6;
+					break;
+				case 'W':
+					ans = 3;
+					break;
+			}
+
+			return ans;
+		}
+
+		public static int Dice6(char c)
+		{
+			int ans;
+			switch (c) {
+				case 'E':
+				default:
+					ans = 4;
+					break;
+				case 'N':
+					ans = 5;
+					break;
+				case 'S':
+					ans = 2;
+					break;
+				case 'W':
+					ans = 3;
+					break;
+			}
+
+			return ans;
+		}
+
+		public static Func<char, int>[] funcs = {
+			Dice1, Dice2, Dice3, Dice4, Dice5, Dice6,
+		};
+
 		public void Exec()
 		{
 			Dice dice = new Dice();
@@ -30,60 +166,21 @@ namespace AtCoder
 				dice.Label[i] = array[i];
 			}
 
-			string str =Console.ReadLine();
-			int topIndex = 1;
-
+			string str = Console.ReadLine();
+			int index = 1;
 			for (var i = 0; i < str.Length; ++i) {
-				switch (str[i]) {
-					case 'E':
-					default:
-						break;
-					case 'N':
-						break;
-					case 'S':
-						break;
-					case 'W':
-						break;
+				if (i == 0) {
+					index = funcs[0](str[i]);
+				} else {
+					index = funcs[index - 1](str[i]);
 				}
+
+				//Console.WriteLine($"{index}");
 			}
-			
+
+			Console.WriteLine($"{dice.Label[index - 1]}");
+
 			Console.ReadKey();
-		}
-	}
-}
-
-namespace Temp {
-	public class QuestionA
-	{
-		void swap(ref long x, ref long y)
-		{
-			long temp = y;
-			y = x;
-			x = temp;
-		}
-
-		public void Exec()
-		{
-			long n = long.Parse(Console.ReadLine());
-			//var array = Console.ReadLine().Split(' ').Select(i => long.Parse(i)).ToArray();
-			var array = Console.ReadLine().Split(' ').Select(i => int.Parse(i)).ToArray();
-
-			Console.Out.Flush();
-
-			Console.ReadLine();
-		}
-	}
-
-	public class QuestionAA
-	{
-		public void Exec()
-		{
-			long n = long.Parse(Console.ReadLine());
-			var array = Console.ReadLine().Split(' ').Select(i => long.Parse(i)).ToArray();
-
-			Console.Out.Flush();
-
-			Console.ReadLine();
 		}
 	}
 }
